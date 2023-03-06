e7bec529ef3ad0c11c0fd4a41e38eb6e947b1cc0
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2023-03-06 11:48:45 +0630

    first

---
+++ latihan1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // begin 
+   // numeric Luas,p,l
+   double Luas, p, l;
+   // display 'Masukkan Panjangnya = '
+   cout << "Masukkan Panjangnya =";
+   // Accept p
+   cin >> p;
+   // display 'Masukkan Lebarnya = '
+   cout << "Masukkan Lebarnya =";
+   // Accept l
+   cin >> l;
+   // compute Luas = p*l
+   Luas = p*l;
+   // display " Luasnya = " + Luas
+   cout << "Luasnya = " << Luas;
+   // end
+}
